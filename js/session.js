3cdd6cf406252340e16a82a26afe7945eb3d9a51
smarak <smaraknibor97@gmail.com>
2026-08-07 11:14:25 +0545

    Remove frontend files and old setup doc from backend branch

--- js/session.js
+++
@@ -1,7 +0,0 @@
-(function tryResumeSession() {
-  const token = localStorage.getItem('kam_token');
-  const userRaw = localStorage.getItem('kam_user');
-  if (token && userRaw) {
-    try { showDashboard(JSON.parse(userRaw)); } catch (e) { /* ignore malformed cache */ }
-  }
-})();
